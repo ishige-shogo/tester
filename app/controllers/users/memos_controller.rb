6e0118a68273a408f77b2c33f94e5f63cf065200
ishige-shogo <shigeshigeishige@gmail.com>
2021-01-03 12:13:48 +0000

    add users controller

---
+++ app/controllers/users/memos_controller.rb
@@ -0,0 +1,2 @@
+class Users::MemosController < ApplicationController
+end
---
+++ test/controllers/users/memos_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class Users::MemosControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
